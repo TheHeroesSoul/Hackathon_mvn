05a468ae4d943a1bbf02baa5a28a0cef97c01258
TheHeroesSoul <lorenzo.lista24@gmail.com>
2025-07-11 12:35:06 +0200

    Update ModelManager.java

--- src/main/java/model/ModelManager.java
+++ src/main/java/model/ModelManager.java
@@ -3,15 +3,28 @@ package main.java.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe ModelManager gestisce una lista di utenti e fornisce metodi per interagire con essa.
+ */
 public class ModelManager {
     private List<Utente> utenti;
 
+    /**
+     * Costruttore della classe ModelManager.
+     * Inizializza la lista di utenti con alcuni utenti predefiniti.
+     */
     public ModelManager() {
         utenti = new ArrayList<>();
         utenti.add(new Utente(1, "pippo", "pippo@email.com", "Pippo", "Rossi"));
         utenti.add(new Utente(2, "anna", "anna@email.com", "Anna", "Bianchi"));
     }
 
+    /**
+     * Trova un utente nella lista in base al nome utente.
+     *
+     * @param username Il nome utente da cercare.
+     * @return L'oggetto Utente corrispondente al nome utente, oppure null se non trovato.
+     */
     public Utente findUtenteByUsername(String username) {
         for (Utente u : utenti) {
             if (u.getUsername().equalsIgnoreCase(username)) {
@@ -20,5 +33,4 @@ public class ModelManager {
         }
         return null;
     }
-
 }
