3c9e1c78de746047da855f86de2561f721f3bf5c
Giulio Giangrande <55993612+imgg98@users.noreply.github.com>
2025-07-12 12:01:43 +0200

    modifiche

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -6,4 +6,24 @@ public class Home {
     private JButton logoutButton;
     private JList list1;
     private JButton creaHackathonButton;
+
+    public Home() {
+        JFrame frame = new JFrame("Home");
+        frame.setContentPane(new JPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        logoutButton = new JButton("Logout");
+        creaHackathonButton = new JButton("Crea Hackathon");
+
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.add(logoutButton);
+        panel.add(creaHackathonButton);
+
+    }
+
+    public static void main(String[] args) {
+        new Home();
+    }
 }
